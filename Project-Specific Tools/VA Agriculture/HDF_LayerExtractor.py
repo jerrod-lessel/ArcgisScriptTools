66f6c3ec6c13b5afbd589ed337079efa53a4c10e
Quinten Geddes <develop.toolbox@gmail.com>
2013-05-10 15:03:57 -0400

    LayerExtractor edits 10.0 toolboxes

--- Project-Specific Tools/VA Agriculture/HDF_LayerExtractor.py
+++ Project-Specific Tools/VA Agriculture/HDF_LayerExtractor.py
@@ -6,7 +6,6 @@
 #                   NASA DEVELOP Program
 # Created:     22/03/2013
 
-
 #-------------------------------------------------------------------------------
 import arcpy
 import numpy as np
@@ -19,34 +18,57 @@ harray=arcpy.GetParameterAsText(1)
 layerlist=arcpy.GetParameterAsText(2)
 outputfolder=arcpy.GetParameterAsText(3)
 outputprefix=arcpy.GetParameterAsText(4)
-excludeZero=arcpy.GetParameterAsText(4)
+excludeZero=arcpy.GetParameter(5)
 layers=layerlist.split(";")
 
 f=h5py.File(hfile)
 
-src=gdal.Open("C:\\Users\\qgeddes\\Downloads\\VAwinery\\LST_MODIS_Aqua\\Analysis\\ReferenceTemplate.tif")
-driver=gdal.GetDriverByName("GTiff")
+#These are the spatial reference parameters that are specific to this grid
+#these parameters will be used to georeference the image
+cellSize=926.6254331
 
-cols,rows=src.RasterXSize,src.RasterYSize
+#this is the Well Known Text for the MODIS Sinusoidal Projection
+sr= """PROJCS["Sinusoidal",
+            GEOGCS["GCS_Undefined",
+                DATUM["D_Undefined",
+                    SPHEROID["User_Defined_Spheroid",6371007.181,0.0]],
+                PRIMEM["Greenwich",0.0],
+                UNIT["Degree",0.017453292519943295]],
+            PROJECTION["Sinusoidal"],
+            PARAMETER["False_Easting",0.0],
+            PARAMETER["False_Northing",0.0],
+            PARAMETER["Central_Meridian",0.0],
+            UNIT["Meter",1.0]]"""
 
-for day in layers:
-    #creating output files
+#this point represtents the left-bottom corner of the dataset
+pnt=arcpy.Point(-7470454.24127,4062325.89888)
 
-    outfile=driver.CreateCopy("{0}\\{1}{2}.tif".format(outputfolder,outputprefix,day),src,0)
+#for each index provided by the Layer List parameter
+for i in layers:
+    outputfilename="{0}\\{1}{2}.tif".format(outputfolder,outputprefix,i)
+    #attaining the data array from the input .h5 file
+    data=f[harray][:,:,(int(i)-1)]
 
-    #attaining the original array and deriving the bad pixel info and masking the original
 
-    data=f[harray][:,:,(int(day)-1)]
+    #converting the array to an arcpy Raster object
+    #the corner point and cellSize information is used here
+    output=arcpy.NumPyArrayToRaster(data,pnt, cellSize,cellSize)
 
+    #defining the output raster's projection as MODIS Sinusoidal
+    arcpy.DefineProjection_management(output, sr)
+##
+##    -----------------------
+##    Extra functions can be added here
+##
+##    ------------------------
 
-    #writing the arrays to the output raster and setting the null values
-    outfile.GetRasterBand(1).WriteArray(data)
-    if excludeZero=="true":
-        outfile.GetRasterBand(1).SetNoDataValue(0)
+    #if exclude Zero is true. Zeros are converted to
+    if excludeZero==True:
+        arcpy.CopyRaster_management(output,outputfilename,nodata_value=0)
+    else:
+        #calculating statisitcs in order to reset color table
+        arcpy.CalculateStatistics_management(output)
+        output.save(outputfilename)
 
-    outfile=0
-    #calculating statisitcs in order to reset color table
-    arcpy.CalculateStatistics_management("{0}\\{1}{2}.tif".format(outputfolder,outputprefix,day))
-
-    arcpy.AddMessage(day)
+    arcpy.AddMessage(i)
 
